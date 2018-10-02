f7781ff816724d3c8e09f42d03d95230e2f8293d
Osman <erenay.osman@gmail.com>
2018-10-01 19:22:40 -0500

    routes with using switch

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 var http = require('http');
-var url = require('url');
-
-function responseHandler(req, res) {
-var queryData = url.parse(req.url, true).query;
-console.log(queryData);
-res.writeHead(200, {"Content-Type": "text/plain"});
-if (queryData.name) {
-// user told us their name in the GET request, ex: http://localhost:1337/?name=Tom
-res.end('Hello ' + queryData.name + '\n');
-} else {
-res.end("Hello World\n");
-}
-}
-var server = http.createServer(responseHandler);
-server.listen(1337);
+http.createServer(function(req, res){
+    var path = req.url;
+    console.log(path);
+    switch(path){
+        case '/':
+        res.writeHead(200, {"Content-Type": "text/plain"});
+        res.end('Homepage');
+        case '/about':
+        res.writeHead(200, {"Content-Type": "text/plain"});
+        res.end('About');
+        default:
+        res.writeHead(404, {"Content-Type": "text/plain"});
+        res.end('Not Found');
+    }
+}).listen(1337);
 console.log('Server running at http://127.0.0.1:1337/');
